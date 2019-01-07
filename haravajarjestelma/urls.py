caaeeea20af465b593558d2748ae7c22df1e1fa9
Tuomas Haapala <tuomas.haapala@anders.fi>
2019-01-07 13:22:29 +0200

    Add geo query API endpoint
    The endpoint is used by doing a GET request with query params "lat" and "lon".
    It returns data of the neighborhood (if any) the given point is in, and data
    of the closest address to the point.

--- haravajarjestelma/urls.py
+++ haravajarjestelma/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from areas.api import NeighborhoodViewSet
+from areas.api import GeoQueryViewSet, NeighborhoodViewSet
 from events.api import EventViewSet
 
 router = DefaultRouter()
 router.register('event', EventViewSet)
 router.register('neighborhood', NeighborhoodViewSet, base_name='neighborhood')
-
+router.register('geo_query', GeoQueryViewSet, base_name='geo_query')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
