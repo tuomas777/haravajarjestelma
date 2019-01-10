8995e58ab13bbb41ff615d7683c9fd12727ffeba
Tuomas Haapala <tuomas.haapala@anders.fi>
2019-01-10 22:03:47 +0200

    Add is_official and is_contractor flags to User model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from helusers.models import AbstractUser
+
+from areas.models import ContractZone
 
 
 class User(AbstractUser):
-    pass
+    is_official = models.BooleanField(verbose_name=_('official'), default=False)
+    is_contractor = models.BooleanField(verbose_name=_('contractor'), default=False)
+
