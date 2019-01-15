4ae4d5e5cd339bc6830b98d267781666e8f41842
Henri Kotkanen <henri.kotkanen@anders.fi>
2019-01-15 17:15:36 +0200

    add datetime validations regarding start and end times

--- events/api.py
+++ events/api.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers, viewsets
 
 from events.models import Event
@@ -7,6 +11,20 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+
+    def validate(self, data):
+        start_time = data.get('start_time', None)
+        end_time = data.get('end_time', None)
+
+        # PATCH updates only 'state', so check that start and end times are present in the data
+        if (start_time and end_time) and (start_time > end_time):
+            raise serializers.ValidationError(_('Event must start before ending.'))
+
+        # TODO should the 7 day minimum waiting time be configurable from e.g. admin?
+        # discussion can be had in https://github.com/City-of-Helsinki/haravajarjestelma/issues/8
+        if start_time and (start_time < timezone.now() + timedelta(days=7)):
+            raise serializers.ValidationError(_('Event cannot start earlier than a week from now.'))
+        return data
 
 
 class EventViewSet(viewsets.ModelViewSet):
