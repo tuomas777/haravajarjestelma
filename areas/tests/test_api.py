0d0dc021b822a75e8a0720a690bfe268f9a091f7
Tuomas Haapala <tuomas.haapala@anders.fi>
2019-01-18 16:15:59 +0200

    Add neighborhood and geo_query API endpoint tests

--- areas/tests/test_api.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from rest_framework.reverse import reverse
-
-
-@pytest.mark.django_db
-def test_smoke(client):
-    response = client.get(reverse('v1:neighborhood-list'))
-    assert response.status_code == 200
